c7167b69f88f0d96f1b8ca9be7a3edc0725723a2
marcussantiago <marcussantiago@student.tafesa.edu.au>
2023-04-03 12:41:19 +0930

    Added the delivery charge $25

--- ProductMaintenance/ProductApps/Product.cs
+++ ProductMaintenance/ProductApps/Product.cs
@@ -14,6 +14,7 @@ namespace ProductApps
         private decimal delivery;
         private decimal wrapping;
         private decimal gst;
+        
 
         private decimal Price
         {
@@ -64,6 +65,7 @@ namespace ProductApps
         public void calTotalPayment()
         {
             TotalPayment = Price * Quantity;
+           
         }
     }
 }
